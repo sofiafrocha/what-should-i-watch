9d09f0d88d70f604537f5acd4b592e7a13ed5aba
Mariana <marianne.lage@gmail.com>
2015-04-22 22:33:22 +0100

    Graph now has labels <3

---
+++ js/bolas2.js
@@ -0,0 +1,73 @@
+var data = {"friends_list": {
+    "Sofia": 18, 
+    "Joaquina": 15, 
+    "Emanuelina": 55
+}};
+
+(function() { 
+	
+  // D3 Bubble Chart 
+
+	var diameter = 600;
+
+	var svg = d3.select('#chart').append('svg')
+		.attr('width', diameter)
+		.attr('height', diameter);
+
+	var bubble = d3.layout.pack()
+		.size([diameter, diameter])
+		.value(function(d) {return d.size;}) // new data is loaded to bubble layout
+		.padding(3);
+
+	//function drawBubbles() {
+
+		// generate data with calculated layout values
+		var nodes = bubble.nodes(processData(data))
+			.filter(function(d) { return !d.children; }); // filter out the outer bubble
+
+		// assign new data to existing DOM 
+		var vis = svg.selectAll('circle')
+			.data(nodes, function(d) { return d.name; });
+		/*
+		vis.enter().append('circle')
+			.attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+			.attr('r', function(d) { return d.r; })
+			.attr('class', function(d) { return d.className; })
+    }*/
+    
+            
+        var elem = svg.selectAll("g bubbleText")
+            .data(processData(data));
+
+        var elemEnter = vis.enter()
+            .append("g")
+            //.attr("transform", function(d){return "translate("+d.x+",80)"});
+
+        var circle = elemEnter.append("circle")
+            .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+            .attr("r", function(d){return d.r} )
+            .attr("stroke","black")
+            .attr('class', function(d) { return d.className; });
+
+        elemEnter.append("text")
+            .attr('transform', function(d) { return 'translate(' + d.x + ',' + d.y + ')'; })
+            .attr("dx", function(d){return -20})
+            .text(function(d){return d.name});
+
+
+        function processData(data) {
+            if(!data) return;
+
+            var obj = data.friends_list;
+
+            var newDataSet = [];
+
+            for(var prop in obj) {
+                newDataSet.push({name: prop, className: prop.toLowerCase().replace(/ /g,''), size: obj[prop]});
+            }
+            return {children: newDataSet};
+        }
+
+            //drawBubbles();
+
+})();
