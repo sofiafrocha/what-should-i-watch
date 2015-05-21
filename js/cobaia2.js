674e3fa959305eadba64daa3a733b9f59bc1d407
Sofia F. Rocha <sofiaroc.pt@gmail.com>
2015-05-21 03:07:43 +0100

    when you click a movie suggestion, it makes a request to the MovieDB API

--- js/cobaia2.js
+++ js/cobaia2.js
@@ -35,7 +35,7 @@ function resgate() {
         }
 
         friendsJSON.push(temp);
-        console.log(temp);
+        //console.log(temp);
     }
     console.log("i'm done.");
 }
@@ -43,7 +43,7 @@ function resgate() {
 // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {
     console.log('statusChangeCallback');
-    console.log(response);
+    //console.log(response);
     // The response object is returned with a status field that lets the
     // app know the current login status of the person.
     // Full docs on the response object can be found in the documentation
--- js/filmes.js
+++ js/filmes.js
@@ -1,34 +1,40 @@
-
 var filmes = new Array();
+var clickedMovieName;
 
-console.log("PART II - Startiiiiing");
+var clickedMovieInfo;
+
+//// Events
+
+$( ".movie-suggestions" ).click(function(e) {
+    clickedMovieName = $( e.target ).html();
+    //console.log("clickedMovieName: " + clickedMovieName);
+    getMovieInfo(clickedMovieName);
+});
+
+function getMovieInfo(input){
+    console.log("PART II - 1");
 
     var url = 'http://api.themoviedb.org/3/',
         mode = 'search/movie?query=',
         input,
         key = '&api_key=b4ab8b4bc3e18c2d31a3641078d88a38';
 
+    $.ajax({
+        type: 'GET',
+        url: url + mode + input + key,
+        async: false,
+        jsonpCallback: 'testing',
+        contentType: 'application/json',
+        dataType: 'jsonp',
+        success: function(json) {
+            console.log(json);
+            console.log("PART II - getMovieInfo - API Responded");
 
-function getMovieInfo(){
-    console.log("PART II - 1");
-    
-        var input = "Pride and Prejudice";
-        $.ajax({
-            type: 'GET',
-            url: url + mode + input + key,
-            async: false,
-            jsonpCallback: 'testing',
-            contentType: 'application/json',
-            dataType: 'jsonp',
-            success: function(json) {
-                console.log(json);
-                console.log("PART II - 1B");
-            },
-            error: function(e) {
-                console.log(e.message);
-            }
-        });
+            clickedMovieInfo = json;
+            console.log("PART II - " + clickedMovieInfo.results[0].title);
+        },
+        error: function(e) {
+            console.log(e.message);
+        }
+    });
 }
-
-
-getMovieInfo();
